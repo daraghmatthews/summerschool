f1b06f800b67a4a93dfc7b6445807afd7ea4b3b3
daragh <daraghmatthews@gmail.com>
2018-06-27 12:09:15 +0300

    small changes during day 1

--- programming/c/datatypes-expressions/expressions.c
+++ programming/c/datatypes-expressions/expressions.c
@@ -3,11 +3,28 @@
 int main(int argc, char *argv[])
 {
     // declare integers, floats, and doubles
+	int a = 1, b = 3, c = 5;
+	int i = 17;
+	float f = 5.0;
+	double d = 6.9987;	
 
 
     // evaluate expressions, e.g. c = a + 2.5 * b
     // and print out results. Try to combine integers, floats
     // and doubles and investigate what happens in type conversions
+	
+	a = (float) 2.5 * i;
+	b = b * f;
+
+
+
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	printf("c = %d\n", c);
+	printf("i = %d\n", i);
+
+	printf("f = %.2f\n", f);
+	printf("d = %.8f\n", d); 	
 
     return 0;
 }
--- unix/job_script.sh
+++ unix/job_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH -J my_job_name
+#SBATCH -J daraghs_job
 #SBATCH -o %J.out
 #SBATCH -e %J.err
 #SBATCH -n 24
 #SBATCH -p small
 #SBATCH -t 5
 #SBATCH --reservation=summer_school
+aprun -n 24 ./prog
 
 
